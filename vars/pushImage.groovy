0ca914ad2666d3fe9150c53b69829e50aad30780
Deepanshu Yadav <56504637+dipuyadav@users.noreply.github.com>
2025-04-30 17:40:37 +0530

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,8 +1,8 @@
 def call() {
-    withCredentials([usernamePassword(credentialsId: 'huaweicloud-SWR-creds', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        sh '''
-            sudo docker push ${REGISTRY}/${REPO}:{env.NEXT_TAG}
-            sudo docker logout ${REGISTRY}
-        '''
-    }
+    sh '''
+        sudo docker login -u "la-north-2@${USERNAME}" -p "${PASSWORD}" ${REGISTRY}
+        sudo docker push ${REGISTRY}/${REPO}:${BUILD_ID}
+        sudo docker logout ${REGISTRY}
+    '''
 }
+
