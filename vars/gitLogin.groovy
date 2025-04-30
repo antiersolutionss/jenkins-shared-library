9ee884bcbd0b0ef8ddc38800490b792580587f63
antiersolutionss <happy@antiersolutions.com>
2025-04-30 11:57:44 +0530

    Update gitLogin.groovy

--- vars/gitLogin.groovy
+++ vars/gitLogin.groovy
@@ -1,5 +1,4 @@
 def call(String branch, String credentialsId, String repoUrl) {
-   
-        git branch: branch, credentialsId: credentialsId, url: repoUrl
-    
+    git branch: branch, credentialsId: credentialsId, url: repoUrl
+    echo "Successfully logged in and cloned the Git repository"
 }
