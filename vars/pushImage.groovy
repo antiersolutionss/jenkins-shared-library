d0680db3f1b24e2db627dd5b99a70d7bd5bfcb62
Deepanshu Yadav <56504637+dipuyadav@users.noreply.github.com>
2025-04-30 17:56:53 +0530

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,6 +1,6 @@
-def call(String repoUrl, String nextTag, String registry) {
+def call(String repoUrl, String nextTag, String dockerRepoUrl) {
     sh """
         sudo docker push ${repoUrl}:${nextTag}
-        
+        sudo docker logout ${dockerRepoUrl}
     """
 }
