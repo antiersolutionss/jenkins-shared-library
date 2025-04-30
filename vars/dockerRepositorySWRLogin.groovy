6a70bac7c9b0620e92965c8ede50799cb9875653
antiersolutionss <happy@antiersolutions.com>
2025-04-30 12:19:33 +0530

    Update dockerRepositorySWRLogin.groovy

--- vars/dockerRepositorySWRLogin.groovy
+++ vars/dockerRepositorySWRLogin.groovy
@@ -2,6 +2,7 @@ def dockerRegistrySWRLogin(String credentialsId, String registry) {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'USR', passwordVariable: 'PSW')]) {
         sh """
             docker login -u $USR -p $PSW ${registry}
+            echo "Logged in Registry successfully"
         """
     }
 }
