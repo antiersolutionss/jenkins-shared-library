8e0d74dbf217a7752c135f6c4ca9d212a54b3f75
antiersolutionss <happy@antiersolutions.com>
2025-04-30 12:32:00 +0530

    Update and rename dockerRepositorySWRLogin.groovy to dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,4 +1,4 @@
-def dockerRegistrySWRLogin(String credentialsId, String registry) {
+def dockerRegistryswrLogin(String credentialsId, String registry) {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'USR', passwordVariable: 'PSW')]) {
         sh """
             docker login -u $USR -p $PSW ${registry}
