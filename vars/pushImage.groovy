48b1b5ddc32b2340ef4b83a5bb80a3962215edc2
Deepanshu Yadav <56504637+dipuyadav@users.noreply.github.com>
2025-04-30 17:40:54 +0530

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,6 +1,5 @@
 def call() {
     sh '''
-        sudo docker login -u "la-north-2@${USERNAME}" -p "${PASSWORD}" ${REGISTRY}
         sudo docker push ${REGISTRY}/${REPO}:${BUILD_ID}
         sudo docker logout ${REGISTRY}
     '''
