2777d54278482c1a464717dfdfca6937de69f153
Deepanshu Yadav <56504637+dipuyadav@users.noreply.github.com>
2025-04-30 17:22:28 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "Building Docker image: env.NEXT_TAG "
     sh "ls"
-    sh "sudo docker build -t env.REPO_URL:env.NEXT_TAG ."
+    sh "sudo docker build -t ${env.REPO_URL}:${env.NEXT_TAG} ."
     echo "IMAGE BUILT SUCCESSFULLY!!"
 }
