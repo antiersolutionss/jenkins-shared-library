7443df3958bdc46c2f6bd391d4d2cf7f4242d094
Deepanshu Yadav <56504637+dipuyadav@users.noreply.github.com>
2025-05-06 11:47:33 +0530

    Update loadEnv.groovy

--- vars/loadEnv.groovy
+++ vars/loadEnv.groovy
@@ -1,26 +1,18 @@
 def call(String credentialsId) {
     withCredentials([file(credentialsId: credentialsId, variable: 'ENV_FILE')]) {
-        // Copy the secret file into the workspace
-         sh "sudo cp \"$ENV_FILE\" .env"
+        sh '''
+            # Copy the secret file
+            sudo cp "$ENV_FILE" .env
 
-        // Load environment variables from .env file
-        def envFile = readFile('.env')
-        def envVars = []
-        envFile.split('\n').each { line ->
-            def keyValue = line.split('=')
-            if (keyValue.size() == 2) {
-                envVars.add("${keyValue[0].trim()}=${keyValue[1].trim()}")
-            }
-        }
+            # Transform it into a shell-exportable script
+           sudo  awk -F ': ' '{gsub(/'\''/, "", $2); print "export " $1 "=" $2}' .env > env_export.sh
 
-        // Export the variables for use in following pipeline steps
-        envVars.each { envVar ->
-            def (key, value) = envVar.tokenize('=')
-            env[key] = value
-        }
+            # Make it executable
+            sudo chmod +x env_export.sh
 
-        echo "Environment variables loaded from ${credentialsId}."
-         // List all environment variables
-        sh "printenv"
+            # Optional: show the generated file
+            echo "Transformed environment file:"
+            cat env_export.sh
+        '''
     }
 }
