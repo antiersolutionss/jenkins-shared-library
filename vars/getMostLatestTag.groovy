4473fd3db50fc8b0586adc57759bce8070f6d3c0
Deepanshu Yadav <56504637+dipuyadav@users.noreply.github.com>
2025-05-06 16:08:54 +0530

    Update getMostLatestTag.groovy

--- vars/getMostLatestTag.groovy
+++ vars/getMostLatestTag.groovy
@@ -16,7 +16,7 @@ def call(Map args) {
                     --creds="\${SWR_USER}:\${SWR_PASSWORD}" \
                     docker://${args.repoUrl} \
                     | jq -r '.Tags[] | select(. != "latest")' \
-                    | sort | tail -1
+                    | sort -V | tail -1
             """, returnStdout: true).trim()
 
             // If there are no tags, return "1.0.0"
@@ -24,13 +24,16 @@ def call(Map args) {
                 echo "No tags found, returning 1.0.0"
                 return "1.0.0"
             }
-          echo "Tags found: ${tags}"
-            // Check if the latest tag matches semantic versioning format
-            // Regular expression for x.y.z format (e.g., 1.2.3)
-            def semanticVersionPattern = /^\d+\.\d+\.\d+$/
+            
+            echo "Tags found: ${tags}"
+            
+            // Check if the tag matches semantic versioning format (with optional v prefix)
+            // Regular expression for vx.y.z or x.y.z format (e.g., v1.0.1 or 1.0.1)
+            def semanticVersionPattern = /^v?\d+\.\d+\.\d+$/
 
             if (tags ==~ semanticVersionPattern) {
-                return tags
+                // If tag starts with 'v', remove it before returning
+                return tags.startsWith('v') ? tags.substring(1) : tags
             } else {
                 echo "Latest tag is not in semantic version format, returning 1.0.0"
                 return "1.0.0"
@@ -41,4 +44,3 @@ def call(Map args) {
         }
     }
 }
-
